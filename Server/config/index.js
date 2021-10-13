f081e718f87fb67046d8e62c8b4e7d4b49eb9bd8
Galit Solomon <71221828+galasolo14@users.noreply.github.com>
2021-10-13 17:06:51 +0300

    Delete index.js

--- Server/config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    SECRET: 'my_secret_hash_paswwort!@#!@#',
-    passwordHash: 'my_secret&@#$@!#$',
-    MongoURI: 'mongodb+srv://gala:1414@cluster0.qqbhm.mongodb.net/shopping'
-}
